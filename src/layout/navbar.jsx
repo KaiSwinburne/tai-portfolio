0f39af8e3548d7e8dd24b638be8d4d016517d540
Thanh Tai Tran <104090322@student.swin.edu.au>
2026-07-22 13:30:08 +1000

    Added navigation menu for mobile view

--- src/layout/navbar.jsx
+++ src/layout/navbar.jsx
@@ -1,4 +1,6 @@
 import { Button } from "@/components/button"
+import { Menu, X } from "lucide-react"
+import { useState } from "react"
 const navLinks = [
     {href: "#skills", label: "Skills"},
     {href: "#experience", label: "Experience"},
@@ -6,12 +8,14 @@ const navLinks = [
 ]
 
 export const Navbar = () => {
+    const [isMobileMenuOpen, setIsMobileMenuOpened] = useState(false);
+
     return <header className="fixed top-0 left-0 right-0 bg-transparent py-5">
         <nav className="container mx-auto px-6 flex items-center justify-between">
             <a href="#" className="text-4xl font-bold tracking-tight font-space-grotesk cur">THANH TAI TRAN</a>
 
             {/*Desktop View Nav Items*/}
-            <div className="flex items-center gap-1">
+            <div className="hidden md:flex items-center gap-1">
                 <div className="px-2 py-1 flex items-center gap-1">
                     {/* Map the links label in the navLinks object into <a> */}
                     {navLinks.map((link, index) => (
@@ -19,14 +23,38 @@ export const Navbar = () => {
                                                         text-foreground hover:text-nav-items-blurred">
                             {link.label}
                         </a>
-                    ))}
-
-                    
+                    ))}                   
                 </div>
             </div>
-            <div>
-                <Button className="text-2xl bg-highlight hover:bg-highlight/90 shadow-lg shadow-highlight/25" px="px-4" py="py-2 hover:cursor-pointer">Contact Me</Button>
+            {/*Contact Button */}
+            <div className="hidden md:block">
+                <Button className="text-2xl bg-highlight hover:bg-highlight/90 shadow-lg shadow-highlight/25 hover:cursor-pointer" px="px-4" py="py-2">Contact Me</Button>
             </div>
+
+            {/*Mobile Hamburger logo*/}
+            <button className="md:hidden p-2 text-foreground hover:cursor-pointer" 
+                    onClick={() => setIsMobileMenuOpened((prev) => !prev)}>
+                {/* ? is tenerary function, opens "X" if true, Menu if false */}
+                {isMobileMenuOpen ? <X/> : <Menu size={24}/>} 
+            </button>
         </nav>
+
+        {/*Mobile View Items*/}
+        {/*In this context, && is the conditional rendering operator, renders the right-side code if true, empty if false */}
+        {isMobileMenuOpen && (
+            <div className="md:hidden glass animation-fade-in">
+                <div className="container mx-auto px-6 py-6 flex flex-col gap-4">
+                    {navLinks.map((link, index) => (
+                        <a href={link.href} 
+                            key={index} 
+                            className="text-xl font-space-grotesk py-2
+                                    text-foreground hover:text-nav-items-blurred">
+                            {link.label}
+                        </a>
+                    ))}
+                    <Button className="text-xl bg-highlight hover:bg-highlight/90 shadow-lg shadow-highlight/25 hover:cursor-pointer" px="px-4" py="py-2">Contact Me</Button>
+                </div>    
+            </div>
+        )}
     </header>
 }
